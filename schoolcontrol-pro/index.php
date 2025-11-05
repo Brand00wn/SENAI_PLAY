63f5127b997182e2661c122f21b14345a7d992da
Daniel Ferraz da Silva Ferreira <man0057917@aluno.firjansenaisesi.com.br>
2025-11-05 19:23:14 -0300

    Adicionando index principal do site

---
+++ schoolcontrol-pro/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8" />
+  <title>School Contro Pro</title>
+  <link rel="stylesheet" href="assets/CSS/style.css" />
+</head>
+<body>
+    <header>
+        <h1>Sistema de Cadastro Acadêmico</h1>
+    </header>
+  <nav>
+    <ul>
+      <li><a href="index.php" class="active">Inicio</a></li>
+      <li><a href="" >Alunos</a></li>
+      <li><a href="" >Turmas</a></li>
+      <li><a href="">Disciplinas</a></li>
+      <li><a href="">Professores</a></li>
+      <li><a href="">Matrículas</a></li>
+     
+    </ul>
+  </nav>
+
+  <main>
+    <p>(Texto de apresentação do projeto)</p>
+  </main>
+
+  <script src="assets/JS/script.js" defer></script>
+</body>
+</html>
